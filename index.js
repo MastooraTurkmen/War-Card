5235de62aeaef599dfa2e9f4ede4d326b373d5af
MastooraTurkmen <janmastoorajan@gmail.com>
2023-10-12 08:32:13 +0430

    set up

--- index.js
+++ index.js
@@ -44,8 +44,6 @@ drawCardBtn.addEventListener("click", async () => {
     }
 })
 
-
-
 function determineCardWinner(card1, card2) {
     const valueOptions = ["2", "3", "4", "5", "6", "7", "8", "9", 
     "10", "JACK", "QUEEN", "KING", "ACE"]
